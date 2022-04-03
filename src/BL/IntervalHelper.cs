00b4c71c7f70205da7a8f3e382e399886535a01e
LandyshH <86878681+LandyshH@users.noreply.github.com>
2022-04-03 19:45:30 +0300

    point 1

---
+++ src/BL/IntervalHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace BL;
+
+public static class IntervalHelper
+{
+    //Построение интервального ряда (Минимум, Максимум, Количество интервалов)
+    public static int NumberIntervals(this IEnumerable<double> source, bool isRoundUp = false) 
+    {
+        var n = source.Count();
+        var m = 1 + 3.322 * System.Math.Log10(n);
+        
+        return isRoundUp ? (int) System.Math.Ceiling(m) : (int) System.Math.Floor(m);
+    }
+
+    public static double IntervalWidth(this IEnumerable<double> source)
+    {
+        var max = source.Max();
+        var min = source.Min();
+        var m = source.NumberIntervals();
+        var k = (max - min) / m;
+        return k;
+    }
+}
