8972a8f3981de0650fcd5c521595a19ea05c05e7
TimSon777 <dusalimov.tima@mail.ru>
2022-04-03 19:48:14 +0300

    ADD: MS

---
+++ src/BL/Exceptions/NoElementsException.cs
@@ -0,0 +1,7 @@
+﻿namespace BL.Exceptions;
+
+public class NoElementsException : Exception
+{
+    public NoElementsException(string? paramName = null) : base(paramName)
+    { }
+}
---
+++ src/BL/MS.cs
@@ -0,0 +1,34 @@
+﻿using Ardalis.GuardClauses;
+using BL.Exceptions;
+
+namespace BL;
+
+// ReSharper disable once InconsistentNaming
+public static class MS
+{
+    public static double Average(this IEnumerable<double> source)
+    {
+        var sum = .0;
+        var count = 0;
+        
+        foreach (var e in Guard.Against.Null(source, nameof(source)))
+        {
+            sum += e;
+            count++;
+        }
+
+        return sum / Guard.Against.Zero(count, nameof(source));
+    }
+
+    public static double StandardDeviation(this double source) 
+        => Math.Sqrt(Guard.Against.Negative(source));
+
+    // public static double Median(this IEnumerable<Interval> source)
+    // {
+    //     var orderedIntervals = source.OrderBy(e => e.Count);
+    //     foreach (var interval in intervals)
+    //     {
+    //         
+    //     }
+    // }
+}
